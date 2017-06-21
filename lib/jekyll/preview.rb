46ceeb6f7d12f4b5684e70893dd15a2e18e066c7
Jacob Paul <jacob@jacobpaul.io>
2017-06-21 14:13:04 -0500

    Add `jekyll preview` command to render pages on demand
    Adds a `preview` subcommand to Jekyll via its commands API (which uses
    Mercenary). `jekyll preview` is similar to `jekyll serve`, but instead
    of rendering the whole site by default every time the command is
    called, it doesn't render anything until a request is made, and then
    it only renders the requested page.
    
    This is accomplished in two parts: first, extending Jekyll's existing
    Serve command (and it's internal 'Servlet') to pass the current URL
    path to Jekyll's Regenerator (part of the existing incremental
    regeneration feature), and then extending Regenerator to tell Jekyll
    that it should only render and write the file matching the URL.
    
    This approach has the advantage of relying largely on Jekyll's already
    tested features. Notably, the stages of Jekyll's build process prior
    to render still function on the whole site, so pages that rely on data
    loaded from other files (like an index of blog posts) should still
    render correctly.
    
    At the moment this is a very alpha release; it's only really been
    tested with a default `jekyll new` site. It also calls the build
    command from start to finish for every request, though it's probable
    that it could at least cache the Site instance rather than create a
    new one for every request, and possible that it could perform the
    earlier build stages (like read) only once.

---
+++ lib/jekyll/preview.rb
@@ -0,0 +1,70 @@
+require "jekyll/preview/version"
+
+module Jekyll
+  module Preview
+
+    # Extend Jekyll's built-in Regenerator to only render and write files matching preview_path,
+    # which we will set on-demand for each request.
+    module Regenerator
+      def regenerate?(document)
+        if super && site.config["preview_path"]
+          site.config["preview_path"] == document.url
+        end
+      end
+    end
+
+    # Jekyll extends WEBrick::HTTPServlet::FileHandler internally, so we can extend it again to call
+    # Build with the preview_path.
+    module Servlet
+      def do_GET(req, res)
+        build_opts = @jekyll_opts.merge("preview_path" => req.path, "quiet" => true)
+
+        # TODO: Could probably cache the instance of Site and call #process on that instead
+        Jekyll::Commands::Build.process(build_opts)
+        super
+      end
+    end
+
+    # Servlet isn't required/loaded until Serve#setup is called, so we need to prepend there.
+    module Serve
+      def setup(destination)
+        super
+        Jekyll::Commands::Serve::Servlet.prepend(Servlet)
+      end
+    end
+
+    class Command < Jekyll::Command
+
+      class << self
+        def init_with_program(prog)
+          prog.command(:preview) do |cmd|
+            cmd.action do |argv, opts|
+              extend_serve_command
+              extend_regenerator
+              register_hooks
+
+              opts["destination"] ||= "_site_preview"
+
+              Jekyll::Commands::Serve.process(opts)
+            end
+          end
+        end
+
+        def extend_serve_command
+          Jekyll::Commands::Serve.singleton_class.prepend(Serve)
+        end
+
+        def extend_regenerator
+          Jekyll::Regenerator.prepend(Regenerator)
+        end
+
+        def register_hooks
+          Jekyll::Hooks.register [:pages, :posts, :documents], :post_write do |doc|
+            puts "Previewing #{doc.url} from #{doc.relative_path}"
+          end
+        end
+      end
+
+    end
+  end
+end
---
+++ lib/jekyll/preview/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Preview
+    VERSION = "0.1.0"
+  end
+end
